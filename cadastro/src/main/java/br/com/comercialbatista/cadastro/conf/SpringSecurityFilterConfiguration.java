790c9d3864ea6797a63d3bce8e64e7ad5e7ff110
rodrigo.carvalho <rodrigo.carvalho@comercialbatista.com.br>
2017-06-13 09:59:49 -0300

    primeiro commit

---
+++ cadastro/src/main/java/br/com/comercialbatista/cadastro/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.comercialbatista.cadastro.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer
+{
+
+}
